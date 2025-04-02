d4f107f3b645010690ba832b9df3c3be9c413b12
Kyle Newman <kyle@kyles-air.dhcp.nd.edu>
2025-04-02 14:55:23 -0400

    more routing

--- feature4-ogrimald-knewman2/src/Components/Auth/Auth.jsx
+++ feature4-ogrimald-knewman2/src/Components/Auth/Auth.jsx
@@ -1,19 +1,29 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-// import { checkUser } from "./AuthService";
+import { checkUser } from "./AuthService";
 
 const AuthModule = () => {
   const navigate = useNavigate();
 
   // redirect already authenticated users back to home
+  useEffect(() => {
+    if (checkUser()) {
+      alert("You are already logged in");
+      navigate("/auth");
+    }
+  }, [navigate]);
 
   return (
     <div>
+      <Link to="/auth/register">
         <button>Register</button>
+      </Link>
       <br />
       <br />
+      <Link to="/auth/login">
         <button>Login</button>
+      </Link>
     </div>
   );
 };
--- feature4-ogrimald-knewman2/src/Components/Components.jsx
+++ feature4-ogrimald-knewman2/src/Components/Components.jsx
@@ -3,6 +3,9 @@ import MemeList from "./MemeList/MemeList.jsx";
 import Header from "./Header/Header.jsx"
 import GameList from "./GameList/GameList.jsx"
 import AuthModule from "./Auth/Auth.jsx";
+import AuthRegister from "./Auth/AuthRegister.jsx";
+import AuthLogin from "./Auth/AuthLogin.jsx";
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute.jsx";
 
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
@@ -10,6 +13,9 @@ export default function Components () {
     return (
         <Routes>
             <Route path="/" element={<AuthModule />} />
+            <Route path="/auth" element={<AuthModule />} />
+            <Route path="/auth/register" element={<AuthRegister />} />
+            <Route path="/auth/login" element={<AuthLogin />} />
             <Route path="/home" element={<Home />} />
             <Route path="/memes" element={<MemeList />} />
             <Route path="/games" element={<GameList />} />
